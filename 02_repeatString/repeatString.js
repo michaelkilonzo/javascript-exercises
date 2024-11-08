327766e944e4bbb564a4e079e5bc32ee2d57f159
Michael Kilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2024-11-08 12:01:51 -0500

    Ex 1, 2, 3 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let res = "";
+    if (times < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < times; i ++) {
+        res += word;
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var revString = "";
 
+    for (i = string.length - 1; i >= 0; i--) {
+        revString += string[i];
+    }
+    return revString;
+    
 };
 
 // Do not edit below this line
