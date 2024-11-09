f065b1a3b20d154fe8b82999d3ae966f651f1559
Michael Kilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2024-11-09 12:30:30 -0500

    Ex 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(temp) {
+  const c = ((temp - 32) * 5/9); 
+  return round(c);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const f = ((9/5 * temp) + 32); 
+  return round(f);
+
 };
+
+// round to 1 decimal place 
+function round(num) {
+  return Math.round(num * 10) / 10;
+}
+
 
 // Do not edit below this line
 module.exports = {
