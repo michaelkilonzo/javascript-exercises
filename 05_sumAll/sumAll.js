8c97bb7125c32578436baae9b062ed6a901a9722
Michael Kilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2024-11-09 12:03:13 -0500

    Ex5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(i, j) {
+    if (!Number.isInteger(i) || !Number.isInteger(j) || i < 0 || j < 0) {
+        return 'ERROR';
+    }
 
+    var sum = 0;
+    const start = Math.min(i, j);
+    const end = Math.max(i, j);
+
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
